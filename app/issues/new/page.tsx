57cc3182e37794e5fd4295c8310871471312b55c
coderAndrew <omolloandrew37@gmail.com>
2025-08-31 10:24:34 +0300

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
-import "easymde/dist/easymde.min.css";
-import SimpleMDE from "react-simplemde-editor";
-import { useForm, Controller } from "react-hook-form";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
 import axios from "axios";
+import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
 
 interface IssueForm {
   title: string;
@@ -12,22 +14,43 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
+  const [error, setError] = useState<string | null>(null);
   const router = useRouter();
   const onSubmit = async (data: IssueForm) => {
     try {
       await axios.post("/api/issues", data);
       router.push("/issues");
-    } catch (error) {
+    } catch {
       console.error("Error creating issue:", error);
+      setError("Failed to create issue");
     }
   };
+
+  // Clear error after 3 seconds
+
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => setError(null), 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
 
   const { register, handleSubmit, control } = useForm<IssueForm>();
 
   return (
     <form className=" max-w-xl space-y-3" onSubmit={handleSubmit(onSubmit)}>
       <TextField.Root placeholder="Title" {...register("title")} />
-
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Icon>
+            <InfoCircledIcon />
+          </Callout.Icon>
+          <Callout.Text>
+            <b>Error</b>
+            <div>{error}</div>
+          </Callout.Text>
+        </Callout.Root>
+      )}
       <Controller
         name="description"
         control={control}
