fb18a0f4c8dad16f0432649c64504a7529190b44
coderAndrew <omolloandrew37@gmail.com>
2025-09-03 09:55:15 +0300

    disable ssr

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import dynamic from "next/dynamic";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 import { createIssueSchema } from "@/app/validationSchemas";
@@ -9,8 +10,10 @@ import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import SimpleMDE from "react-simplemde-editor";
 import { z } from "zod";
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
